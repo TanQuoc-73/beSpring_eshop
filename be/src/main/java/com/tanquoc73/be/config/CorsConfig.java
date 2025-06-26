41daf1308f2ed93f179eb91d7ea7078718679f12
TanQuoc-73 <cutorasusu2004@Gmail.com>
2025-06-26 22:02:40 +0700

    'brands'

---
+++ be/src/main/java/com/tanquoc73/be/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.tanquoc73.be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import jakarta.annotation.Nonnull;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@Nonnull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
